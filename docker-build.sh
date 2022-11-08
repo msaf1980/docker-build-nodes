e56864d92529c436688ec6250e29feb210f278d0
msaf1980 <msaf1980@gmail.com>
2022-11-09 02:29:57 +0500

    fix strip version

--- docker-build.sh
+++ docker-build.sh
@@ -16,13 +16,26 @@ FAIL=0
 [ -z "${CI_COMMIT_REF_NAME}" ] && CI_COMMIT_REF_NAME="$( git symbolic-ref HEAD 2> /dev/null | cut -b 12- )"
 [ -z "${CI_USER_NAME}" ] && CI_USER_NAME="$( git config user.email )"
 
-IMAGE="`echo ${IMAGE_TAG} | awk ' match($0, /^(.+)\.[0-9\.]+$/, p) { printf "%s", p[1] } '`" 
-[ "${IMAGE}" == "" -o "${IMAGE}" == ":" ] && {
-  echo "parse IMAGE_TAG ${IMAGE_TAG} return ${IMAGE}, IMAGE_TAG must be in format NAME-VERSION" >&2
+NAME="`echo ${IMAGE_TAG} | sed -E 's/-[0-9\.]+$//'`" 
+IMAGE="`echo ${IMAGE_TAG} | sed -E 's/\.[0-9]+$//'`" 
+LANG="`echo ${IMAGE_TAG} | awk -F'-' '{ n=NF-1 ; print $n } '`" 
+VERSION="`echo ${IMAGE_TAG} | awk -F'-' '{ print $NF  } '`" 
+[ "${NAME}" == "" -o "${NAME}" == "${IMAGE_TAG}" ] && {
+  echo "parse IMAGE_TAG ${IMAGE_TAG} return NAME ${NAME}, IMAGE_TAG must be in format OS-LANG-VERSION.VERSION_MINOR, dir OS-LANG-VERSIONR must exist" >&2
   exit 1
 }
-LANG="`echo ${IMAGE_TAG} | awk -F'-' ' match($0, /^.*-([a-z]+)-([0-9\.]+)$/, p) { printf "%s_version=%s", p[1], p[2] } '`" 
-VERSION="`echo ${IMAGE_TAG} | awk -F'-' ' match($0, /^.*-([a-z]+)-([0-9\.]+)$/, p) { printf "%s", p[2] } '`" 
+[ "${IMAGE}" == "" -o "${IMAGE}" == "${IMAGE_TAG}" ] && {
+  echo "parse IMAGE_TAG ${IMAGE_TAG} return IMAGE ${IMAGE}, IMAGE_TAG must be in format OS-LANG-VERSION.VERSION_MINOR, dir OS-LANG-VERSIONR must exist" >&2
+  exit 1
+}
+[ "${LANG}" == "" -o "${LANG}" == "${IMAGE_TAG}" ] && {
+    echo "parse IMAGE_TAG return LANG $LANG, IMAGE_TAG must be in format OS-LANG-VERSION" >&2
+    exit 1
+}
+[ "${VERSION}" == "" -o "${VERSION}" == "${IMAGE_TAG}" ] && {
+    echo "parse IMAGE_TAG return VERSION ${VERSION}, IMAGE_TAG must be in format OS-LANG-VERSION" >&2
+    exit 1
+}
 
 DIR="docker-${IMAGE}"
 [ -d "${DIR}" ] || {
@@ -31,14 +44,15 @@ DIR="docker-${IMAGE}"
 }
 
 [ -z "${REGISTRY_URL}" ] && {
-  PUSH_URL="${REGISTRY_PROJECT}:${VERSION}"
+  PUSH_URL="${REGISTRY_PROJECT}/${NAME}:${VERSION}"
 } || {
-  PUSH_URL="${REGISTRY_URL}/${REGISTRY_PROJECT}:${VERSION}"
+  PUSH_URL="${REGISTRY_URL}/${REGISTRY_PROJECT}/${NAME}:${VERSION}"
 }
-echo ${PUSH_URL} from ${DIR}
+LANG_VERSION="${LANG}_version=${VERSION}"
+echo "${PUSH_URL} from ${DIR}, ${LANG_VERSION}"
 
 docker build \
-      --build-arg ${LANG} \
+      --build-arg ${LANG_VERSION} \
       --label "branch=${CI_COMMIT_REF_NAME}" \
       --label "commit=${CI_COMMIT_SHA}" \
       --label "builder=${CI_USER_NAME}" \
